7f21ce31e3e767f9c756753162938120fdbd5297
Stephane Vailland <stephane.vailland@st.com>
2017-08-02 17:59:03 +0100

    first helloworld application version

---
+++ files/hellofunc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <helloworld.h>
+
+void myPrintHelloMake(void) {
+
+  printf("Hello World, this is my first package!\n");
+
+  return;
+}
+
---
+++ files/helloworld.c
@@ -0,0 +1,9 @@
+#include <helloworld.h>
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
+
